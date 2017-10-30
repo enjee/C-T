d268b213e681a0e4703eed0086ea6d809d3c4dd7
Gijs Schouten <gijsschouten2013@gmail.com>
2017-10-30 15:24:05 +0100

    url in filter.py
    url in filter.py

--- python-socket/utilities/Filter.py
+++ python-socket/utilities/Filter.py
@@ -69,6 +69,9 @@ class Filter():
 
     def get_yelpreviews(self, yelp_id):
         reviews = []
+        url = "https://api.yelp.com/v3/businesses/" + yelp_id + "/reviews"
+
+        
         for i in range(len(self.df_restaurants)):
             categories += self.get_categories_from_string(self.df_restaurants['categories'][i])
 
