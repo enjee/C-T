2615fb43f4a8447c17cef79aa00fcd5e60851c4b
njpol <mail@nielsjan.nl>
2017-11-07 15:45:31 +0100

    Change loading of file

--- python-socket/utilities/FileIO.py
+++ python-socket/utilities/FileIO.py
@@ -1,20 +1,21 @@
 import pandas as pd
 
 DATA_FILE = "../data/restaurants.csv"
+data_file = None
+print "loading {}".format(DATA_FILE)
+with open(DATA_FILE) as data_file:
+    data_file = pd.read_csv(data_file, sep='\t', encoding='utf-8')
+
 
 class FileIO():
     def __init__(self, data_handler):
         self.data_handler = data_handler
-        self.data_file = self.load_file()
+        self.data_file = data_file
 
     #######################################################
     # Load a specific JSON file from inside the the DATA_DIR
     #######################################################
-    def load_file(self):
-        print "loading {}".format(DATA_FILE)
-        with open(DATA_FILE) as data_file:
-            data = pd.read_csv(data_file, sep='\t', encoding='utf-8')
-        return data
+
 
     def update_dataset(self):
         self.data_handler.send({"fake_update": True})
